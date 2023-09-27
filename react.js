4a1ddfb97faaa37e5300d348ea7f591b4457b9a6
Daniel Nikolaev <69330641+daniel-nikolaev@users.noreply.github.com>
2023-09-27 19:35:17 -0400

    Delete react.js

--- react.js
+++
@@ -1,9 +0,0 @@
-import { Waveform } from '@uiball/loaders'
-
-export default function PageSection({ isLoading }) {
-  return (
-    <div aria-live="polite" aria-busy={isLoading}>
-      {isLoading && <Waveform />}
-    </div>
-  )
-}
