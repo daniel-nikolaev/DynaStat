b91fb4ca94b25305d757deb256e392c8aac35756
Daniel Nikolaev <69330641+daniel-nikolaev@users.noreply.github.com>
2023-09-28 13:50:32 -0400

    Delete Forms.js

--- Forms.js
+++
@@ -1,7 +0,0 @@
-import { PatientDataCreateForm } from './ui-components';
-
-import { PatientDataUpdateForm } from './ui-components';
-
-import { UserInformationCreateForm } from './ui-components';
-
-import { UserInformationUpdateForm } from './ui-components';
