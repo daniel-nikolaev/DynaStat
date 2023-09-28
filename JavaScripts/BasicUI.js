f3a39c17dec5107659282c7bf1c88d25d8d197e5
Daniel Nikolaev <69330641+daniel-nikolaev@users.noreply.github.com>
2023-09-28 14:37:37 -0400

    Update BasicUI.js

--- JavaScripts/BasicUI.js
+++ JavaScripts/BasicUI.js
@@ -17,9 +17,37 @@ export default function PageSection({ isLoading }) {
 }  
 
 import { Filters } from './ui-components';
+export default function PageSection({ isLoading }) {
+  return (
+    <div aria-live="polite" aria-busy={isLoading}>
+      {isLoading && <Filters />}
+    </div>
+  )
+}
 
 import { Logo } from './ui-components';
+export default function PageSection({ isLoading }) {
+  return (
+    <div aria-live="polite" aria-busy={isLoading}>
+      {isLoading && <Logo />}
+    </div>
+  )
+}
 
 import { NavBarHeader2 } from './ui-components';
+export default function PageSection({ isLoading }) {
+  return (
+    <div aria-live="polite" aria-busy={isLoading}>
+      {isLoading && <NavBarHeader2 />}
+    </div>
+  )
+}
 
 import { SideBar } from './ui-components';
+export default function PageSection({ isLoading }) {
+  return (
+    <div aria-live="polite" aria-busy={isLoading}>
+      {isLoading && <SideBar />}
+    </div>
+  )
+}
