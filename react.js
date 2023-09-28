d39304a82831e3cbebf6b304aaed523e3ada3c2d
Daniel Nikolaev <69330641+daniel-nikolaev@users.noreply.github.com>
2023-09-28 10:38:59 -0400

    Update react.js

--- react.js
+++ react.js
@@ -1,9 +1,16 @@
-import { Waveform } from '@uiball/loaders'
+import { Pinwheel } from '@uiball/loaders'
+
+<Pinwheel 
+ size={35}
+ lineWeight={3.5}
+ speed={1} 
+ color="black" 
+/>
 
 export default function PageSection({ isLoading }) {
   return (
     <div aria-live="polite" aria-busy={isLoading}>
-      {isLoading && <Waveform />}
+      {isLoading && <Pinwheel />}
     </div>
   )
 }
