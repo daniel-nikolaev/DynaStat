a57f157a747e2d4964faccc4ce4842767e98e146
Daniel Nikolaev <69330641+daniel-nikolaev@users.noreply.github.com>
2023-09-28 13:48:56 -0400

    Create Forms.js

---
+++ JavaScripts/Forms.js
@@ -0,0 +1,8 @@
+import { PatientDataCreateForm } from './ui-components';
+
+import { PatientDataUpdateForm } from './ui-components';
+
+import { UserInformationCreateForm } from './ui-components';
+
+import { UserInformationUpdateForm } from './ui-components';
+
