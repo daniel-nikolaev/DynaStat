950083dad24c9174476503d5b06efcbb47342a3c
Daniel Nikolaev <69330641+daniel-nikolaev@users.noreply.github.com>
2023-09-28 14:38:01 -0400

    Update Forms.js

--- JavaScripts/Forms.js
+++ JavaScripts/Forms.js
@@ -1,8 +1,35 @@
 import { PatientDataCreateForm } from './ui-components';
+export default function PageSection({ isLoading }) {
+  return (
+    <div aria-live="polite" aria-busy={isLoading}>
+      {isLoading && <PatientDataCreateForm />}
+    </div>
+  )
+}
 
 import { PatientDataUpdateForm } from './ui-components';
+export default function PageSection({ isLoading }) {
+  return (
+    <div aria-live="polite" aria-busy={isLoading}>
+      {isLoading && <Pinwheel />}
+    </div>
+  )
+}
 
 import { UserInformationCreateForm } from './ui-components';
+export default function PageSection({ isLoading }) {
+  return (
+    <div aria-live="polite" aria-busy={isLoading}>
+      {isLoading && <Pinwheel />}
+    </div>
+  )
+}
 
 import { UserInformationUpdateForm } from './ui-components';
-
+export default function PageSection({ isLoading }) {
+  return (
+    <div aria-live="polite" aria-busy={isLoading}>
+      {isLoading && <Pinwheel />}
+    </div>
+  )
+}
