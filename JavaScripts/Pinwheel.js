a9029a33c094a8b7a20d478a0f99430376753008
Daniel Nikolaev <69330641+daniel-nikolaev@users.noreply.github.com>
2023-09-29 10:54:53 -0400

    Add files via upload

---
+++ JavaScripts/Pinwheel.js
@@ -0,0 +1,16 @@
+import { Pinwheel } from '@uiball/loaders'
+
+<Pinwheel 
+ size={75}
+ lineWeight={3.5}
+ speed={0.75} 
+ color="black" 
+/>
+
+export default function PageSection({ isLoading }) {
+  return (
+    <div aria-live="polite" aria-busy={isLoading}>
+      {isLoading && <Pinwheel />}
+    </div>
+  )
+}
