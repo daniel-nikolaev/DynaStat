2722ea69e11d557ff7bdc7e6af45e6313c49b918
Daniel Nikolaev <69330641+daniel-nikolaev@users.noreply.github.com>
2023-10-10 11:02:18 -0400

    Create Image.js

---
+++ JavaScripts/UIElements/Image.js
@@ -0,0 +1,18 @@
+import { Image } from '@aws-amplify/ui-react';
+
+export const DefaultImageExample = () => {
+  return (
+    <Image
+      alt="DynaStat Logo"
+      src="/Pictures/dynastat-logo.png"
+      objectFit="initial"
+      objectPosition="50% 50%"
+      backgroundColor="initial"
+      height="75%"
+      width="75%"
+      opacity="100%"
+      onClick={() => alert('HEALTHCARE!!!')}
+    />
+  );
+};
+
