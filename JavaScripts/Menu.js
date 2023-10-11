529d69fef8a1b6d322c9f8431a23f7772b7c015b
Daniel Nikolaev <69330641+daniel-nikolaev@users.noreply.github.com>
2023-10-11 08:55:51 -0400

    Create Menu.js

---
+++ JavaScripts/Menu.js
@@ -0,0 +1,18 @@
+import { Menu, MenuItem, IconsProvider, Link } from '@aws-amplify/ui-react';
+import { FiMoreHorizontal } from 'react-icons/fi';
+
+export const MenuIcon = () => (
+  <IconsProvider
+    icons={{
+      menu: {
+        menu: <FiMoreHorizontal />,
+      },
+    }}
+  >
+    <Menu>
+      <MenuItem><Link href="google.com">Google</Link></MenuItem>
+      <MenuItem>Create a Copy</MenuItem>
+      <MenuItem>Mark Draft</MenuItem>
+    </Menu>
+  </IconsProvider>
+);
