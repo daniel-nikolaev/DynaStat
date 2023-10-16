ad948e3d3c22f3d81d5dac2fb0f1027f724ebb9c
Daniel Nikolaev <danielnik2016@gmail.com>
2023-10-16 09:21:24 -0400

    major upddate to css

--- JavaScripts/UIElements/ContactUs.js
+++
@@ -1,9 +0,0 @@
-import { ContactUs } from './ui-components';
-
-export default function PageSection({ isLoading }) {
-  return (
-    <div aria-live="polite" aria-busy={isLoading}>
-      {isLoading && <ContactUs />}
-    </div>
-  )
-}
