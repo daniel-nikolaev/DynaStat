b4c0ef565a010e62cea0cb5634ba9b1eb375982d
Daniel Nikolaev <danielnik2016@gmail.com>
2026-03-21 18:56:03 -0400

    DELETION

--- javascript/UIElements/Logo.js
+++
@@ -1,9 +0,0 @@
-import { Logo } from './ui-components';
-
-export default function PageSection({ isLoading }) {
-  return (
-    <div aria-live="polite" aria-busy={isLoading}>
-      {isLoading && <Logo />}
-    </div>
-  )
-}
